a0f2bf883e3f0dce543ab5a50a47e8f9597ba833
Tyler Haglund <relyt4me@gmail.com>
2020-08-01 16:55:51 -0600

    Create testing suite for Booking

---
+++ test/Booking-test.js
@@ -0,0 +1,49 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Booking from '../src/Booking';
+
+describe('Booking', () => {
+  let booking;
+  before(() => {
+    booking = new Booking('5fwrgu4i7k55hl6sz', 9, '2020/04/22', 15, 358.4);
+  });
+  it('should be a function', () => {
+    expect(Booking).to.be.a('function');
+  });
+
+  it('should be an instance of User', () => {
+    expect(booking).to.be.an.instanceOf(Booking);
+  });
+
+  it('should have an id', () => {
+    expect(booking.id).to.equal('5fwrgu4i7k55hl6sz');
+  });
+
+  it('should have an userID', () => {
+    expect(booking.userID).to.equal(9);
+  });
+
+  it('should have a date', () => {
+    expect(booking.date).to.equal('2020/04/22');
+  });
+
+  it('should have a room number', () => {
+    expect(booking.roomNumber).to.equal(15);
+  });
+
+  it('should have no service charges by default', () => {
+    expect(booking.roomServiceCharges).to.eql([]);
+  });
+
+  it('should have a cost', () => {
+    expect(booking.cost).to.equal(358.4);
+  });
+
+  it('should ensure the cost is a number and return 0 if NaN', () => {
+    const bookingOops = (booking = new Booking('5fwrgu4i7k55hl6sz', 9, '2020/04/22', 15, '358.4'));
+    const bookingOops2 = (booking = new Booking('5fwrgu4i7k55hl6sz', 9, '2020/04/22', 15, 'potatoe'));
+
+    expect(bookingOops.cost).to.equal(358.4);
+    expect(bookingOops2.cost).to.equal(0);
+  });
+});
