9269c9d37c06d10404af89af2e4cf18413f5e34a
Tyler Haglund <relyt4me@gmail.com>
2020-08-01 16:17:32 -0600

    Pass the no name sad path

--- src/Customer.js
+++ src/Customer.js
@@ -3,7 +3,7 @@ import User from '../src/User';
 class Customer extends User {
   constructor(id, name) {
     super(id);
-    this.name = name;
+    this.name = name || 'NO NAME';
     this.bookings = [];
   }
 }
