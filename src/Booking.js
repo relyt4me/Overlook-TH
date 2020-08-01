73439905ed54df8c984e4788001b81d83ef92d44
Tyler Haglund <relyt4me@gmail.com>
2020-08-01 16:59:39 -0600

    Pass argument test id userID date roomNumber

--- src/Booking.js
+++ src/Booking.js
@@ -1,6 +1,9 @@
 class Booking {
-  constructor(id) {
+  constructor(id, userID, date, roomNumber) {
     this.id = id;
+    this.userID = userID;
+    this.date = date;
+    this.roomNumber = roomNumber;
   }
 }
 
