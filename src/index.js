febf17600f69cfdf2c446169b58782559dc21d0f
Tyler Haglund <relyt4me@gmail.com>
2020-07-31 18:27:29 -0600

    Add scss files for all pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
+import './css/_variable.scss';
+import './css/_mixins.scss';
 import './css/base.scss';
+import './css/_login.scss';
+import './css/_customer.scss';
+import './css/_manager.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/calculator-background.jpg';
