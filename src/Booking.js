3077b069a85f6102075244c73f54856be2670b72
Tyler Haglund <relyt4me@gmail.com>
2020-08-01 17:01:39 -0600

    Pass cost and cost sad path for Booking

--- src/Booking.js
+++ src/Booking.js
@@ -1,10 +1,11 @@
 class Booking {
-  constructor(id, userID, date, roomNumber) {
+  constructor(id, userID, date, roomNumber, cost) {
     this.id = id;
     this.userID = userID;
     this.date = date;
     this.roomNumber = roomNumber;
     this.roomServiceCharges = [];
+    this.cost = parseFloat(cost) || 0;
   }
 }
 
