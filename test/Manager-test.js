1d350b6c6ed47fed5bb823283520909ede78e66b
Tyler Haglund <relyt4me@gmail.com>
2020-08-01 18:48:27 -0600

    Creatframework for spies testing suite

--- test/Manager-test.js
+++ test/Manager-test.js
@@ -2,11 +2,25 @@ import chai from 'chai';
 const expect = chai.expect;
 import User from '../src/User';
 import Manager from '../src/Manager';
+const spies = require('chai-spies');
+chai.use(spies);
 
 describe('Customer', () => {
   let manager;
   before(() => {
     manager = new Manager();
+  });
+  beforeEach(() => {
+    global.fetch = () => {
+      return Promise;
+    };
+    Promise.then = () => {
+      return Promise;
+    };
+    Promise.catch = () => {
+      return Promise;
+    };
+    chai.spy.on(global, ['fetch']);
   });
   it('should be a function', () => {
     expect(Manager).to.be.a('function');
@@ -26,7 +40,20 @@ describe('Customer', () => {
     expect(manager.password).to.equal('overlook2020');
   });
 
+  // Spies test to implement later
   it('should be able to create a booking', () => {});
 
+  it('should call fetch once when posting a booking', () => {});
+
+  it('should call then twice when posting a booking', () => {});
+
+  it('should use the correct parameters when posting a booking', () => {});
+
   it('should be able to remove a booking', () => {});
+
+  it('should call fetch once when removing a booking', () => {});
+
+  it('should call then twice when removing a booking', () => {});
+
+  it('should use the correct parameters when removing a booking', () => {});
 });
